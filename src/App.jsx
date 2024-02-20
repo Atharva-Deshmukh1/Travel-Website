f2480097722e395df648d3dd1042f09c32531653
Atharva-Deshmukh1 <atharva25deshmukh@gmail.com>
2024-02-20 19:51:28 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar/Navbar'
 import LandingPage from './Components/LandingPage/LandingPage'
 import Marquee from './Components/Marquee/Marquee'
 import About from './Components/About/About'
+import Eyes from './Components/Eyes/Eyes'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <LandingPage />
       <Marquee />
       <About />
+      <Eyes />
     </div>
     
   )
---
+++ src/Components/Eyes/Eyes.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react'
+
+const Eyes = () => {
+
+    
+const [rotate, setRotate] = useState(0)
+
+useEffect(()=>{
+    window.addEventListener("mousemove" , (e)=>{
+        let mouseX = e.clientX;
+        let mouseY = e.clientY;
+
+        let delX = mouseX - window.innerWidth/2;
+        let delY = mouseY - window.innerHeight/2;
+
+        var angle = Math.atan2(delY, delX) * (180/Math.PI)
+        setRotate(angle-180)
+
+    })
+})
+
+
+  return (
+    <div className='eys w-full h-screen overflow-hidden'>
+        <div className='relative w-full h-full'>
+            <div className='absolute flex gap-10 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]'>
+                <div className='flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-slate-300'>
+                    <div  className=' w-2/3 h-2/3 relative  rounded-full bg-slate-800'>
+                        <div style={{transform:`translate(-50%,-50%) rotate(${rotate}deg)`}} className='line w-full h-10 absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]'>
+                            <div className='w-10 h-10 rounded-full bg-zinc-100'>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+                <div className='flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-slate-300'>
+                    <div className='w-2/3 h-2/3 relative  rounded-full bg-slate-800'>
+                    <div style={{transform:`translate(-50%,-50%) rotate(${rotate}deg)`}} className='line w-full h-10 absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]'>
+                            <div className='w-10 h-10 rounded-full bg-zinc-100'>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Eyes
