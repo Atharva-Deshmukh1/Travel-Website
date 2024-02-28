5d44961feed5ea05b35b131c8e63f2f2aef16c44
Atharva-Deshmukh1 <atharva25deshmukh@gmail.com>
2024-02-28 11:32:20 +0530

    update

--- src/Components/Eyes/Eyes.jsx
+++ src/Components/Eyes/Eyes.jsx
@@ -29,28 +29,28 @@ const Eyes = () => {
             
             <div data-scroll data-scroll-section data-scroll-speed="-.7" className='w-full h-[80%] md:h-full '>
                 
-                <div className='absolute top-20 left-10'>
+                <div className='absolute top-20 left-[1.5rem]'>
                     <Card 
                         image="https://www.axisbank.com/images/default-source/progress-with-us_new/what-to-do-in-switzerland.jpg"
                         Name="Switzerland"
                         desc="Switzerland is a mountainous Central European country, home to numerous lakes, villages and the high peaks of the Alps."
                     />
                 </div>
-                <div className='absolute top-20 right-10'>
+                <div className='absolute top-20 right-[1.5rem]'>
                     <Card 
                         image="https://a.cdn-hotels.com/gdcs/production126/d1421/11d188b3-38f4-4842-88f6-a928f14b5581.jpg?impolicy=fcrop&w=800&h=533&q=medium"
                         Name="Dubai"
                         desc="Dubai is a city and emirate in the United Arab Emirates known for luxury shopping, ultramodern architecture and a lively nightlife scene."
                     />
                 </div>
-                <div className='absolute bottom-20 left-10'>
+                <div className='absolute bottom-20 left-[1.5rem]'>
                     <Card 
                         image="https://im.indiatimes.in/media/content/2015/Jan/maldives-twisted-sifter_1421316320_725x725.jpg?w=725&h=483&cc=1"
                         Name="Maldives"
                         desc="A popular honeymoon destination for Indians, Maldives is a good place to visit if you want an international location teeming with Indians."
                     />
                 </div>
-                <div className='absolute bottom-20 right-10'>
+                <div className='absolute bottom-20 right-[1.5rem]'>
                     <Card 
                         image="https://im.indiatimes.in/media/content/2015/Jan/singapore-marhsall-university_1421316697_725x725.jpg?w=725&h=483&cc=1"
                         Name="Singapore"
